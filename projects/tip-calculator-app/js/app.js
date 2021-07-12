2670c46068eada69ccf5261fa57f81d3b8964742
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2021-07-12 16:25:58 +0300

    continue refactor

---
+++ projects/tip-calculator-app/js/app.js
@@ -0,0 +1,8 @@
+console.log("loaded");
+
+const amountInput = document.querySelector("#amountInput");
+const noPeople = document.querySelector("#card-people");
+const tipPercent = document.querySelectorAll(".card__tip");
+
+const tipPerPerson = document.querySelector(".card__tip-amount--sum");
+const amountPerPerson = document.querySelector(".card__total-amount--sum");
