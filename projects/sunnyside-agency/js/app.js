8e4720406865a4c5a5dae0ab6df897dba0dda7ed
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2021-09-03 21:40:23 +0300

    sunnyside-agengy finished

--- projects/sunnyside-agency/js/app.js
+++ projects/sunnyside-agency/js/app.js
@@ -4,3 +4,11 @@ const menuList = document.querySelector(".menu-list");
 toggleBtn.addEventListener("click", () => {
 	menuList.classList.toggle("hidden");
 });
+
+if (window.innerWidth > 500) {
+	menuList.classList.remove("hidden");
+	toggleBtn.classList.add("hidden");
+} else {
+	menuList.classList.add("hidden");
+	toggleBtn.classList.add("hidden");
+}
